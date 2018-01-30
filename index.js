2f5d8c5e963abe29d09f39d58055df1571c2510d
Vinc <personal@aldovincenti.com>
2018-01-30 19:04:14 +0100

    Released API 1.1

--- index.js
+++ index.js
@@ -12,68 +12,67 @@ let Q = require('q')
 
 module.exports = Botsociety
 
-function Botsociety (config) {
+function Botsociety(config) {
     this.apiUrl = 'https://app.botsociety.io/apisociety'
-    this.apiVersion = config.userId || '1.1'
+    this.apiVersion = config.apiVersion || '1.1'
     this.apiSource = 'npm'
     this.userId = config.userId
     this.apiKey = config.apiKey
-    this.debug = (!config.debug || config.debug === undefined )? false : config.debug
+    this.debug = (!config.debug || config.debug === undefined) ? false : config.debug
 
-    this.call = (url)=>{
+    this.call = (url) => {
         let options = {
-            url : `${this.apiUrl}/${this.apiVersion}/${this.apiSource}/${url}`,
-            method:  'GET',
+            url: `${this.apiUrl}/${this.apiVersion}/${this.apiSource}/${url}`,
+            method: 'GET',
             headers: {
-                'Content-Type' : 'application/json',
-                'user_id' : this.userId,
+                'Content-Type': 'application/json',
+                'user_id': this.userId,
                 'api_key_public': this.apiKey
             }
         }
         let deferred = Q.defer();
-        request(options,(error, response, body) => {
+        request(options, (error, response, body) => {
             this.clog(`RESPONSE STATUS ${response.statusCode}`)
-
-        if(error){
-            this.clog(`ERROR`)
-            this.clog(`${error}`)
-            return deferred.reject(new Error('Error'))
-        }
-        this.clog(`RESPONSE BODY`)
-        this.clog(`${body}`)
-        return deferred.resolve(body)
-    })
+            if (error) {
+                this.clog(`ERROR`)
+                this.clog(`${error}`)
+                return deferred.reject(new Error('Error'))
+            }
+            this.clog(`RESPONSE BODY`)
+            this.clog(`${body}`)
+            return deferred.resolve(body)
+        })
         return deferred.promise
     }
 
-    this.clog = function (message){
-        if(this.debug){
+    this.clog = function (message) {
+        if (this.debug) {
             console.log(message)
         }
     }
 }
 
-Botsociety.prototype.auth = function(){
+Botsociety.prototype.auth = function () {
     this.clog(`CALLING AUTH`)
     return this.call('')
 }
 
-Botsociety.prototype.getConversations = function(conversationId = ''){
+Botsociety.prototype.getConversations = function (conversationId = '') {
     this.clog(`CALLING API CONVERSATIONS/${conversationId}`)
     return this.call(`conversations/${conversationId}`)
 }
 
-Botsociety.prototype.getMessage = function(messageId){
+Botsociety.prototype.getMessage = function (messageId) {
     this.clog(`CALLING API MESSAGE/${messageId}`)
     return this.call(`messages/${messageId}`)
 }
 
-Botsociety.prototype.getMessageByConversation = function(conversationId, messageId){
-    this.clog(`CALLING API MESSAGE/${messageId}`)
+Botsociety.prototype.getMessageByConversation = function (conversationId, messageId) {
+    this.clog(`CALLING API MESSAGEBYCONVERSATION/${messageId}`)
     return this.call(`conversations/${conversationId}/messages/${messageId}`)
 }
 
-Botsociety.prototype.getVariables = function(conversationId){
-    this.clog(`CALLING API VARIABLES/${conversationId}`)
-    return this.call(`variables/${conversationId}`)
+Botsociety.prototype.getVariables = function (conversationId) {
+    this.clog(`CALLING API conversations/${conversationId}/variables`)
+    return this.call(`conversations/${conversationId}/variables/`)
 }
